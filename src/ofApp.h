8283210745507a77689796115c29531f530158ec
samnang sok <samnangsok.se@gmail.com>
2018-05-03 22:34:12 -0700

    initalize the camera position and boolean

--- src/ofApp.h
+++ src/ofApp.h
@@ -10,65 +10,68 @@
 #include "ParticleSystem.h"
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		void drawAxis(ofVec3f);
-		void initLightingAndMaterials();
-		void savePicture();
-		void toggleWireframeMode();
-		void togglePointsDisplay();
-		void toggleSelectTerrain();
-		void setCameraTarget();
-		bool  doPointSelection();
-		void drawBox(const Box &box);
-		Box meshBounds(const ofMesh &);
-		void subDivideBox8(const Box &b, vector<Box>& boxList);
-
-		bool mouseIntersectPlane(ofVec3f planePoint, ofVec3f planeNorm, ofVec3f &point);
-
-		ofEasyCam cam;
-		ofxAssimpModelLoader mars, lander;
-
-		Particle spaceship;
-		ParticleEmitter exhaust;
-		ParticleSystem sys;
-		ThrusterForce thruster;
-		ofSoundPlayer thrust_sound;
-
-		ofMesh marsMesh;
-		ofLight light;
-		Box* boundingBox;
-		vector<vector<Box>> levels;
-	
-		bool bAltKeyDown;
-		bool bCtrlKeyDown;
-		bool bWireframe;
-		bool bDisplayPoints;
-		bool bPointSelected;
-		
-		bool bRoverLoaded;
-		bool bTerrainSelected;
-	
-		ofVec3f selectedPoint;
-		ofVec3f intersectPoint;
-
-
-		const float selectionRange = 4.0;
-
-		TreeNode* octree;
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    void drawAxis(ofVec3f);
+    void initLightingAndMaterials();
+    void savePicture();
+    void toggleWireframeMode();
+    void togglePointsDisplay();
+    void toggleSelectTerrain();
+    void setCameraTarget();
+    bool  doPointSelection();
+    void drawBox(const Box &box);
+    Box meshBounds(const ofMesh &);
+    void subDivideBox8(const Box &b, vector<Box>& boxList);
+    
+    bool mouseIntersectPlane(ofVec3f planePoint, ofVec3f planeNorm, ofVec3f &point);
+    
+    ofEasyCam cam;
+    ofxAssimpModelLoader mars, lander;
+    
+    Particle spaceship;
+    ParticleEmitter exhaust;
+    ParticleSystem sys;
+    ThrusterForce thruster;
+    ofSoundPlayer thrust_sound;
+    
+    ofMesh marsMesh;
+    ofLight light;
+    Box* boundingBox;
+    vector<vector<Box>> levels;
+    
+    bool bAltKeyDown;
+    bool bCtrlKeyDown;
+    bool bWireframe;
+    bool bDisplayPoints;
+    bool bPointSelected;
+    
+    bool bRoverLoaded;
+    bool bTerrainSelected;
+    
+    ofVec3f selectedPoint;
+    ofVec3f intersectPoint;
+    
+    ofVec3f camPosition;
+    ofVec3f downView;
+    bool camMove;
+    bool sideView;
+    const float selectionRange = 4.0;
+    
+    TreeNode* octree;
 };
